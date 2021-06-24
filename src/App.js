045872529765806179f77ca6cb8744e1a3069704
Bektemir <bejandor@gmail.com>
2021-06-24 15:23:27 +0300

    structured the tags with grid layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';// Импортируем из модуля
 import './App.css';
-import Header from './header';
 
-import Technologies from './Technologies';
 
 const App = () => {
   return (
-    <div>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://i.pinimg.com/736x/86/46/2d/86462d25d7919adad32ce94285060cbe.jpg" />
+      </header>
 
-      <Header/>
-      <Technologies />
- 
-    </div >
+      <nav className="nav">
+        <div>
+          <a href="#">Profile</a>
+        </div>
+        <div>
+          <a href="#">Messages</a>
+        </div>
 
+
+        <div>
+          <a href="#">Music</a>
+        </div>
+
+        <div>
+          <a href="#">Settings</a>
+        </div>
+
+      </nav>
+
+      <div className="content">
+        <img src="https://sun9-24.userapi.com/impg/o36MGhZJEJ9KT_JMsOUrrJ51dgeTJQYDTe50zw/mc-3vmwh1c8.jpg?size=894x652&quality=96&proxy=1&sign=8928faad6f96db7ab9516c8b4f279dc6" />
+
+
+        <div>
+          avatar and description
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+        </div>
+
+        <div>
+          <div>
+            Post 1
+          </div>
+
+          <div>
+            Post 2
+          </div>
+        </div>
+
+      </div>
+    </div>
   );
 }
 
 
-export default App; //Для того стобы можно было использовать
+export default App; 
