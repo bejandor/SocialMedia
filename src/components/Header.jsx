dbf358c5878ad7de2514ff8b8714ebbf14527760
Bektemir <bejandor@gmail.com>
2021-06-25 10:44:11 +0300

    added somponents and changed the color of blocks

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Header = () =>{
+    return(
+        <header className="header">
+            <img src="https://i.pinimg.com/736x/86/46/2d/86462d25d7919adad32ce94285060cbe.jpg" />
+        </header>
+    );
+}
+
+export default Header;
