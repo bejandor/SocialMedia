8651ea57285937013bcc1fbf9cf89069de1b8125
Bektemir <bejandor@gmail.com>
2021-06-30 13:01:58 +0300

    Revert "added somponents and changed the color of blocks"
    This reverts commit dbf358c5878ad7de2514ff8b8714ebbf14527760.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';// Импортируем из модуля
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
 
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
+      <header className="header">
+        <img src="https://i.pinimg.com/736x/86/46/2d/86462d25d7919adad32ce94285060cbe.jpg" />
+      </header>
+
+      <nav className="nav">
+        <div>
+          <a href="#">Profile</a>
+        </div>
+        <div>
+          <a href="#">Messages</a>
+        </div>
+
+
+        <div>
+          <a href="#">Music</a>
+        </div>
+
+        <div>
+          <a href="#">Settings</a>
+        </div>
+
+      </nav>
+
+      <div className="content">
+        <img src="https://sun9-24.userapi.com/impg/o36MGhZJEJ9KT_JMsOUrrJ51dgeTJQYDTe50zw/mc-3vmwh1c8.jpg?size=894x652&quality=96&proxy=1&sign=8928faad6f96db7ab9516c8b4f279dc6" />
+
+
+        <div>
+          avatar and description
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+        </div>
+
+        <div>
+          <div>
+            Post 1
+          </div>
+
+          <div>
+            Post 2
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
